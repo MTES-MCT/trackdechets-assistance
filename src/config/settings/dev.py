50821f2d52d44f1d756dc5d702e3b0ea44abc1fa
Laurent Paoletti <lp@providenz.fr>
2022-10-05 20:30:17 +0200

    Fix settings

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -7,9 +7,4 @@ SECRET_KEY = "xyzabcdefghu"
 INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS  # noqa F405
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp-providenzpro.alwaysdata.net"
-EMAIL_HOST_PASSWORD = "supermailer"
-EMAIL_HOST_USER = "mailer@providenz.fr"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-MESSAGE_RECIPIENT = "lp@providenz.fr"
+
